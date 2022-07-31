40237992a1a23b37dfec25c72371f8f87f3c2db2
Saad allah Samadi <ssamadi@e1r13p6.1337.ma>
2022-07-31 16:13:31 +0100

    install node and passport

---
+++ back-end/README.md
@@ -0,0 +1,13 @@
+# install typescript compiler
+
+using node package manger can install tsc(typescript compiler)
+
+
+npm i -g tsc
+
+
+## install nesjs 
+
+npm i -g @nestjs/cli
+
+
---
+++ docker/tools/back-end/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+apt-get -y update
+apt-get -y install curl vim 
+
+# install typescript compiler
+
+#using node package manger can install tsc(typescript compiler)
+
+
+npm i -g tsc
+
+
+## install nesjs 
+
+npm i -g @nestjs/cli
+
+# install Passport strategy for authenticating with 42 using the OAuth 2.0 API.
+
+npm i  -g passport-42
