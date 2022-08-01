cddcaf5abd7431b5de4e50cdf3ba9d8b8919cc28
Mouad Ilmi <milmi@e1r12p3.1337.ma>
2022-08-01 21:11:21 +0100

    Adding chat components

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -3,12 +3,104 @@ import Vue from 'vue';
 import TopBar from './views/TopBar.vue';
 import UserAvatar from './views/UserAvatar.vue';
 import FriendList from './views/FriendList.vue';
-import ChannelList from './views/ChannelList.vue';
+import ChannelList from './views/FriendsStatus.vue';
 
 export default Vue.extend({
     name: "App",
     data: () => ({
-        drawer: null
+        drawer: null,
+        messages: [
+          {
+            from: 'John Josh',
+            message: `Sure, I'll see you later.`,
+            time: '10:42am',
+            color: 'deep-purple lighten-1',
+          },
+          {
+            from: 'John Doe',
+            message: 'Yeah, sure. Does 1:00pm work?',
+            time: '10:37am',
+            color: 'green',
+          },
+          {
+            from: 'John Josh',
+            message: `Sure, I'll see you later.`,
+            time: '10:42am',
+            color: 'deep-purple lighten-1',
+          },
+          {
+            from: 'John Doe',
+            message: 'Yeah, sure. Does 1:00pm work?',
+            time: '10:37am',
+            color: 'green',
+          },
+          {
+            from: 'John Josh',
+            message: `Sure, I'll see you later.`,
+            time: '10:42am',
+            color: 'deep-purple lighten-1',
+          },
+          {
+            from: 'John Doe',
+            message: 'Yeah, sure. Does 1:00pm work?',
+            time: '10:37am',
+            color: 'green',
+          },
+          {
+            from: 'John Josh',
+            message: `Sure, I'll see you later.`,
+            time: '10:42am',
+            color: 'deep-purple lighten-1',
+          },
+          {
+            from: 'John Doe',
+            message: 'Yeah, sure. Does 1:00pm work?',
+            time: '10:37am',
+            color: 'green',
+          },
+          {
+            from: 'John Josh',
+            message: `Sure, I'll see you later.`,
+            time: '10:42am',
+            color: 'deep-purple lighten-1',
+          },
+          {
+            from: 'John Doe',
+            message: 'Yeah, sure. Does 1:00pm work?',
+            time: '10:37am',
+            color: 'green',
+          },
+          {
+            from: 'John Josh',
+            message: `Sure, I'll see you later.`,
+            time: '10:42am',
+            color: 'deep-purple lighten-1',
+          },
+          {
+            from: 'John Doe',
+            message: 'Yeah, sure. Does 1:00pm work?',
+            time: '10:37am',
+            color: 'green',
+          },
+          {
+            from: 'John Josh',
+            message: `Sure, I'll see you later.`,
+            time: '10:42am',
+            color: 'deep-purple lighten-1',
+          },
+          {
+            from: 'John Doe',
+            message: 'Yeah, sure. Does 1:00pm work?',
+            time: '10:37am',
+            color: 'green',
+          },
+          {
+            from: 'John Josh',
+            message: 'Did you still want to grab lunch today?Did you still want to grab lunch today?Did you still want to grab lunch today?Did you still want to grab lunch today?Did you still want to grab lunch today?Did you still want to grab lunch today?Did you still want to grab lunch today?Did you still want to grab lunch today?Did you still want to grab lunch today?Did you still want to grab lunch today?',
+            time: '9:47am',
+            color: 'deep-purple lighten-1',
+          },
+        ],
     }),
     components: { TopBar, UserAvatar, FriendList, ChannelList }
 });
@@ -35,12 +127,37 @@ export default Vue.extend({
 
     <ChannelList />
     <v-main>
-       <!-- Chat Window -->
+
+      <v-container fluid="true" style="height:100%;">
+        <v-row justify="space-around d-flex flex-column">
+          <v-card v-for="message in messages" :key="message.time" flat>              
+                  <v-list-item
+                      :key="message.time"
+                      class=""
+                    >
+                      <v-list-item-avatar class="align-self-start mr-2">                     
+                        <v-avatar size="40">
+                          <v-img src="https://i.pinimg.com/736x/35/d5/0e/35d50ea7dac12997c537c9c7e59e5498.jpg"></v-img>
+                        </v-avatar>                     
+                      </v-list-item-avatar>
+                      <v-list-item-content class="received-message">
+                        <v-card color="grey darken-3" class="flex-none">                        
+                          <v-card-text class="white--text pa-2 d-flex flex-column">
+                            <span class="text-body-2 font-weight-bold">{{message.from}} <span class="text-caption">{{message.time}}</span> </span>                                                 
+                            <span class="align-self-start text-subtitle-1">{{ message.message }}</span>
+                            
+                          </v-card-text>
+                        </v-card>                   
+                      </v-list-item-content>
+                  </v-list-item>
+                </v-card>
+        </v-row>
+      </v-container>
+
     </v-main>
 
     <v-footer
       app
-      color="transparent"
       height="72"
       inset
     >
@@ -52,8 +169,32 @@ export default Vue.extend({
         solo
       ></v-text-field>
     </v-footer>
-    
   </v-app>
 </template>
 
+<style lang="scss" scoped>
+.chat-message {
+  display: unset !important;
+  white-space: break-spaces;
+}
+.chat-screen {
+  max-height: 320px;
+  overflow-y: auto;
+}
+.flex-none {
+  flex: unset;
+}
+.sent-message::after {
+    content: ' ';
+    position: absolute;
+    width: 0;
+    height: 0;
+    left: auto;    
+    right: 54px;
+    top: 12px;
+    bottom: auto;
+    border: 12px solid;
+    border-color: #1976d2 transparent transparent transparent;
+}
+</style>
 
--- front-end/src/views/UserAvatar.vue
+++ front-end/src/views/UserAvatar.vue
@@ -5,25 +5,73 @@
 
         mini-variant
       >
-        <v-avatar
-          class="d-block text-center mx-auto mt-4"
-          color="primary"
-          size="36"
+        <v-hover
+        v-slot="{ hover }"
         >
-          <img
-            alt="Avatar"
-            src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460"
+          <v-avatar
+            class="d-block text-center mx-auto mt-4"
+            color="primary"
+            size="36"
+            :class="{ 'on-hover': hover }"
           >
-        </v-avatar>
+            <v-btn
+              v-bind="attrs"
+              v-on="on"
+              class="settings avatar-padding"
+              min-width="36px"
+              :elevation="1"
+            >
+              <v-icon
+              dark
+              >mdi-cog</v-icon>
+            </v-btn>
 
+            <img
+              :elevation="2"
+              alt="Avatar"
+              src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460"
+            >
+          </v-avatar>
+        </v-hover>
         <v-divider class="mx-3 my-5"></v-divider>
-
-        <v-avatar
-          v-for="n in 6"
-          :key="n"
-          class="d-block text-center mx-auto mb-9"
-          color="grey lighten-1"
-          size="28"
-        ></v-avatar>
+        <v-hover
+        v-slot="{ hover }"
+        v-for="n in 6"
+        :key="n"
+        >
+          <v-avatar
+            class="d-block text-center mx-auto mb-9"
+            color="grey lighten-1"
+            :class="{ 'on-hover': hover }"
+            size="28"
+          ></v-avatar>
+        </v-hover>
+        <v-dialog v-model="dialog" fullscreen scrollable>
+          <v-card>
+            This is a test
+          </v-card>
+        </v-dialog>
       </v-navigation-drawer>
 </template>
+
+<style lang="scss" scoped>
+.v-avatar.on-hover
+{
+  box-shadow: 0 0px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+
+.avatar-padding
+{
+  padding: 0 !important;
+}
+.settings
+{
+  display: none;
+  background-color: rgba(0, 0, 0, 0.2);
+}
+
+.v-avatar.on-hover > .settings
+{
+  display: inline-flex;
+}
+</style>
