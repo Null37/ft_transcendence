7db4f30fbbe818f25e7c89842f65f0653beb8b2e
Mouad Ilmi <milmi@e1r11p5.1337.ma>
2022-08-02 16:22:38 +0100

    Adding content

---
+++ front-end/src/views/Community.vue
@@ -0,0 +1,248 @@
+<script lang="ts">
+import Vue from 'vue';
+import TopBar from '../components/TopBar.vue';
+import UserAvatar from '../components/UserAvatar.vue';
+import FriendList from '../components/FriendList.vue';
+import ChannelList from '../components/FriendsStatus.vue';
+
+export default Vue.extend({
+    name: "App",
+    mounted: function () {
+        this.loaded()
+    },
+    methods: {
+        loaded: function () {
+            console.log(this.$refs.scrooll);
+        }
+    },
+    data: () => ({
+        drawer: null,
+        messages: [
+          {
+            id: 0,
+            from: 'John Josh',
+            message: `Sure, I'll see you later.`,
+            time: '10:42am',
+            color: 'deep-purple lighten-1',
+          },
+          {
+            id: 1,
+            from: 'John Doe',
+            message: 'Yeah, sure. Does 1:00pm work?',
+            time: '10:37am',
+            color: 'green',
+          },
+          {
+            id: 2,
+            from: 'John Josh',
+            message: `Sure, I'll see you later.`,
+            time: '10:42am',
+            color: 'deep-purple lighten-1',
+          },
+          {
+            id: 3,
+            from: 'John Doe',
+            message: 'Yeah, sure. Does 1:00pm work?',
+            time: '10:37am',
+            color: 'green',
+          },
+          {
+            id: 4,
+            from: 'John Josh',
+            message: `Sure, I'll see you later.`,
+            time: '10:42am',
+            color: 'deep-purple lighten-1',
+          },
+          {
+            id: 5,
+            from: 'John Doe',
+            message: 'Yeah, sure. Does 1:00pm work?',
+            time: '10:37am',
+            color: 'green',
+          },
+          {
+            id: 6,
+            from: 'John Josh',
+            message: `Sure, I'll see you later.`,
+            time: '10:42am',
+            color: 'deep-purple lighten-1',
+          },
+          {
+            id: 7,
+            from: 'John Doe',
+            message: 'Yeah, sure. Does 1:00pm work?',
+            time: '10:37am',
+            color: 'green',
+          },
+          {
+            id: 8,
+            from: 'John Josh',
+            message: `Sure, I'll see you later.`,
+            time: '10:42am',
+            color: 'deep-purple lighten-1',
+          },
+          {
+            id: 9,
+            from: 'John Doe',
+            message: 'Yeah, sure. Does 1:00pm work?',
+            time: '10:37am',
+            color: 'green',
+          },
+          {
+            id: 10,
+            from: 'John Josh',
+            message: `Sure, I'll see you later.`,
+            time: '10:42am',
+            color: 'deep-purple lighten-1',
+          },
+          {
+            id: 11,
+            from: 'John Doe',
+            message: 'Yeah, sure. Does 1:00pm work?',
+            time: '10:37am',
+            color: 'green',
+          },
+          {
+            id: 12,
+            from: 'John Josh',
+            message: `Sure, I'll see you later.`,
+            time: '10:42am',
+            color: 'deep-purple lighten-1',
+          },
+          {
+            id: 13,
+            from: 'John Doe',
+            message: 'Yeah, sure. Does 1:00pm work?',
+            time: '10:37am',
+            color: 'green',
+          },
+          {
+            id: 14,
+            from: 'John Josh',
+            message: 'Did you still want to grab lunch today?Did you still want to grab lunch today?Did you still want to grab lunch today?Did you still want to grab lunch today?Did you still want to grab lunch today?Did you still want to grab lunch today?Did you still want to grab lunch today?Did you still want to grab lunch today?Did you still want to grab lunch today?Did you still want to grab lunch today?',
+            time: '9:47am',
+            color: 'deep-purple lighten-1',
+          },
+        ],
+    }),
+    components: { TopBar, UserAvatar, FriendList, ChannelList }
+});
+</script>
+
+
+<template>
+   <v-app id="inspire">
+    <TopBar />
+
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      width="300"
+    >
+      <UserAvatar />
+
+      <v-sheet
+        height="128"
+        width="100%"
+      >
+      
+        <v-list
+          class="pl-14"
+          shaped
+        >
+          <v-list-item
+            link
+          >
+            <v-list-item-content>
+              <v-list-item-title>Play a Game</v-list-item-title>
+            </v-list-item-content>
+            
+          </v-list-item>
+          <v-list-item
+            link
+          >
+            <v-list-item-content>
+              <v-list-item-title>Community</v-list-item-title>
+            </v-list-item-content>
+            
+          </v-list-item>
+        </v-list>
+      
+      </v-sheet>
+      <FriendList />
+    </v-navigation-drawer>
+
+    <ChannelList />
+    <v-main>
+
+      <v-container fluid style="height:100%;" v-scroll:#scrooll="0">
+        <v-row justify="space-around d-flex flex-column">
+          <v-card v-for="message in messages" :key="message.time" flat>              
+                  <v-list-item
+                      :key="message.time"
+                      class=""
+                    >
+                      <v-list-item-avatar class="align-self-start mr-2">                     
+                        <v-avatar size="40">
+                          <v-img src="https://i.pinimg.com/736x/35/d5/0e/35d50ea7dac12997c537c9c7e59e5498.jpg"></v-img>
+                        </v-avatar>                     
+                      </v-list-item-avatar>
+                      <v-list-item-content class="received-message">
+                        <v-card color="grey darken-3" class="flex-none">                        
+                          <v-card-text class="white--text pa-2 d-flex flex-column">
+                            <span ref="scrooll" v-if="message.id == messages.length - 1"  class="text-body-2 font-weight-bold">{{message.from}} <span class="text-caption">{{message.time}}</span> </span>  
+                            <span v-else class="text-body-2 font-weight-bold">{{message.from}} <span class="text-caption">{{message.time}}</span> </span>                                              
+                            <span class="align-self-start text-subtitle-1">{{ message.message }}</span>
+                            
+                          </v-card-text>
+                        </v-card>                   
+                      </v-list-item-content>
+                  </v-list-item>
+                </v-card>
+        </v-row>
+      </v-container>
+
+    </v-main>
+
+    <v-footer
+      app
+      height="72"
+      inset
+    >
+      <v-text-field
+        dense
+        flat
+        hide-details
+        rounded
+        solo
+      ></v-text-field>
+    </v-footer>
+  </v-app>
+</template>
+
+<style lang="scss" scoped>
+.chat-message {
+  display: unset !important;
+  white-space: break-spaces;
+}
+.chat-screen {
+  max-height: 320px;
+  overflow-y: auto;
+}
+.flex-none {
+  flex: unset;
+}
+.sent-message::after {
+    content: ' ';
+    position: absolute;
+    width: 0;
+    height: 0;
+    left: auto;    
+    right: 54px;
+    top: 12px;
+    bottom: auto;
+    border: 12px solid;
+    border-color: #1976d2 transparent transparent transparent;
+}
+</style>
+
---
+++ front-end/src/views/Login.vue
@@ -0,0 +1,53 @@
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+    data: () => ({drawer: null})
+});
+</script>
+
+<template>
+<div id="app">
+  <v-app id="inspire">
+   <v-main>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <div class="text-center">
+                <v-btn
+                color="white"
+                class="black--text"
+                x-large
+                >
+
+                <v-avatar
+                    size="36"
+                    tile
+                    left
+                >
+                    <img
+                    :elevation="2"
+                    alt="Avatar"
+                    src="../assets/logo.png"
+                    >
+                </v-avatar>
+
+                Login
+                </v-btn>
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</div>
+</template>
