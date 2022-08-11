8d5d358ea1deb4ca166df7f366dcebfc386c68e9
Saad allah Samadi <42317207+Null37@users.noreply.github.com>
2022-08-11 11:28:46 +0100

    add callbaclurl

--- clear.sh
+++ clear.sh
@@ -1,11 +1,11 @@
-sudo docker-compose down
-#sudo rm -rf /var/lib/docker/volumes/elk-stack_certs
-sudo docker stop $(sudo docker ps -qa);
-sudo docker rm -f $(sudo docker ps -qa);
-# sudo docker rmi -f node;
-# sudo docker system prune -f
+ docker-compose down
+# rm -rf /var/lib/docker/volumes/elk-stack_certs
+ docker stop $( docker ps -qa);
+ docker rm -f $( docker ps -qa);
+#  docker rmi -f node;
+#  docker system prune -f
 
 
-sudo docker volume rm $(sudo docker volume ls -q);
-sudo docker network rm $(sudo docker network ls -q) 2>/dev/null
-sudo docker-compose up --build
+ docker volume rm $( docker volume ls -q);
+ docker network rm $( docker network ls -q) 2>/dev/null
+ docker-compose up --build
