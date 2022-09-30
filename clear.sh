c15a9464b9119fa13d0d49995a4672a0dff8c9a2
abel-haj <abel-haj@student.1337.ma>
2022-09-30 16:36:02 +0100

    cleaning everything and building anew

--- clear.sh
+++ clear.sh
@@ -9,14 +9,13 @@ else
         PERM=""
 fi
 
- $PERM docker-compose down
-# rm -rf /var/lib/docker/volumes/elk-stack_certs
- $PERM docker stop $($PERM docker ps -qa);
- $PERM docker rm -f $($PERM docker ps -qa);
-#  docker rmi -f node;
-#  docker system prune -f
+$PERM docker-compose down
+$PERM docker stop $($PERM docker ps -qa) 2>/dev/null;
+$PERM docker rm -f $($PERM docker ps -qa) 2>/dev/null;
+$PERM docker rmi $($PERM docker images -qa); # slows building time, not necessary # but to be sure
+$PERM docker system prune -f
 
 
- $PERM docker volume rm $($PERM docker volume ls -q);
- $PERM docker network rm $($PERM docker network ls -q) 2>/dev/null
- $PERM docker-compose up --build
+$PERM docker volume rm $($PERM docker volume ls -q) 2>/dev/null;
+$PERM docker network rm $($PERM docker network ls -q) 2>/dev/null;
+$PERM docker-compose up --build
