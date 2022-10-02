310d0de6d6fb1fc12c51c17a5f5020708da6477f
abel-haj <abel-haj@student.1337.ma>
2022-10-02 02:40:19 +0100

    independant queues in front end

--- front-end/src/views/Game.vue
+++ front-end/src/views/Game.vue
@@ -24,7 +24,7 @@ export default Vue.extend({
     name: "App",
 
     methods: {
-    changeAvatarC: function(newavatar)
+    changeAvatarC: function(newavatar: any)
     {
       this.avatar = newavatar;
     },
@@ -68,6 +68,7 @@ export default Vue.extend({
         console.log('EMITTING JOIN');
         
         this.gameSocket.emit('joinQueue');
+        this.isSpeedyLoading = false;
         this.isLoading = true;
       },
       emitCancel() {
@@ -82,6 +83,7 @@ export default Vue.extend({
         console.log('EMITTING JOIN');
         
         this.gameSocket.emit('joinSpeedyQueue');
+        this.isLoading = false;
         this.isSpeedyLoading = true;
       },
       emitSpeedyCancel() {
@@ -90,17 +92,17 @@ export default Vue.extend({
         this.gameSocket.emit('cancelSpeedyQueue');
         this.isSpeedyLoading = false;
       },
-	  statusChanged(data)
+	  statusChanged(data: any)
 	  {
 
-      console.log("dattta === ", data, " | friendlist length == ", this.friendlist.length);
-      console.log(this.friendlist);
+      // console.log("dattta === ", data, " | friendlist length == ", this.friendlist.length);
+      // console.log(this.friendlist);
       for (let i = 0; i < this.friendlist.length; i++)
       {
         if (this.friendlist[i].username === data.username)
         {
           this.friendlist[i].status = data.status;
-          console.log("this.friendlist[i].status == ", this.friendlist[i].status ," | data.status == ", data.status, " | data.username == ", data.username);
+          // console.log("this.friendlist[i].status == ", this.friendlist[i].status ," | data.status == ", data.status, " | data.username == ", data.username);
           return ;
         }
       }
